0fbb2a3ac4a9528fd6380864343ab117e66cd0e1
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2024-05-23 13:15:22 +0200

    chore: added comments

--- src/util/Auth/index.js
+++ src/util/Auth/index.js
@@ -1,15 +1,16 @@
-import { initializeApp, } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 
 // Web App's Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyACO67fP1fkY1mFzT2vJgw4CrqoYeCD6Sw",
-    authDomain: "class-work-9cbca.firebaseapp.com",
-    projectId: "class-work-9cbca",
-    storageBucket: "class-work-9cbca.appspot.com",
-    messagingSenderId: "739558401618",
-    appId: "1:739558401618:web:42e1a44180239d35bb618b"
+  // Preferrably hide your config, anyone has access to push to your firestore, if they can see your config!
+  apiKey: "AIzaSyACO67fP1fkY1mFzT2vJgw4CrqoYeCD6Sw",
+  authDomain: "class-work-9cbca.firebaseapp.com",
+  projectId: "class-work-9cbca",
+  storageBucket: "class-work-9cbca.appspot.com",
+  messagingSenderId: "739558401618",
+  appId: "1:739558401618:web:42e1a44180239d35bb618b",
 };
 
 // Initialize Firebase
