33810431120a2e9decc28a24aaed6151890a0ea1
Eddie <21100419@virtualwindow.co.za>
2024-06-17 00:03:46 +0200

    Register user

--- src/screens/Onboarding/Register/RegisterForm/index.js
+++ src/screens/Onboarding/Register/RegisterForm/index.js
@@ -2,9 +2,12 @@ import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-nativ
 import React, { useState } from 'react'
 import { GlobalStyle } from '../../../../util/Style';
 import { useNavigation } from '@react-navigation/native';
+import { Register } from '../../../../util/Auth/Services/register.service';
 
 export const RegisterForm = () => {
 
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(true);
 
@@ -13,8 +16,12 @@ export const RegisterForm = () => {
         navigation.navigate('OnboardingStack', { screen: screen });
     };
 
-    const handleRegister = () => {
+    const goToChooseAvatar = (screen) => {
         navigation.navigate('ChooseAvatar', { screen: 'OnboardinStack' });
+    };
+
+    const handleRegister = () => {
+       Register(email,username,password,goToChooseAvatar)
     }
 
     return (
@@ -22,23 +29,39 @@ export const RegisterForm = () => {
 
             <View style={styles.header}>
                 <Text style={styles.headerText}>
-                    Register
-                    to Compete
+                    Register to Compete
                 </Text>
             </View>
 
             <View style={styles.formWrap}>
                 <View style={styles.inputWrap}>
-                    <TextInput style={[styles.inputText]} placeholder='Username' />
+                    <TextInput
+                     style={[styles.inputText]} 
+                     placeholder='Username' 
+                     onChangeText={value => setUsername(value)}
+                     />
                 </View>
                 <View style={styles.inputWrap}>
-                    <TextInput style={[styles.inputText]} placeholder='Email address' />
+                    <TextInput 
+                    style={[styles.inputText]} 
+                    placeholder='Email address'
+                    onChangeText={value => setEmail(value)}
+                     />
                 </View>
                 <View style={styles.inputWrap}>
-                    <TextInput style={[styles.inputText]} placeholder='Password' secureTextEntry={showPassword} />
+                    <TextInput 
+                    style={[styles.inputText]} 
+                    placeholder='Password' 
+                    secureTextEntry={showPassword} 
+                    />
                 </View>
                 <View style={styles.inputWrap}>
-                    <TextInput style={[styles.inputText]} placeholder='Retry Password' secureTextEntry={showPassword} />
+                    <TextInput 
+                    style={[styles.inputText]} 
+                    placeholder='Retry Password' 
+                    secureTextEntry={showPassword} 
+                    onChangeText={value => setPassword(value)}
+                    />
                 </View>
             </View>
 
--- src/util/Auth/Services/register.service.js
+++ src/util/Auth/Services/register.service.js
@@ -1,20 +1,42 @@
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { config } from "../../Services/firebase";
+import { createItem } from "../../Services/Data";
+import { getUsertLocation } from "../../Services/Map/getUserLocation";
+import { Timestamp } from "firebase/firestore";
 
-export const Register = (email, password, callBackFunction) => {
+export const Register = (email, username, password, callBackFunction) => {
 
     createUserWithEmailAndPassword(config, email, password)
-        .then((userCredential) => {
-            // After user is cretaed do the following
+        .then(async(userCredential) => {
+            
+            // After user is cretaed do the following:
 
             const user = userCredential.user;
+
+            const location = await getUsertLocation()
+            const JoinedOn = Timestamp.now()
+            const userDetails = {
+                email: user.email,
+                username: username,
+                // dateJoined: JoinedOn,
+                role: 'player',
+                region: location,
+                avatar: '1',
+                dragonballs: [],
+                awards:[]
+            }
+
+            const registerPlayer = await createItem("players", userDetails);
             callBackFunction();
+            
+            return true
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
 
             console.log(errorMessage)
+            return false
         });
 
 };
